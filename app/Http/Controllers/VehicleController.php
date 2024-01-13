a5ee626909571d69021f6bf62e3522154fc19ef9
sazzad7299 <sazzad7299@users.noreply.github.com>
2024-01-13 19:59:19 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Vehicle;
 use App\Http\Requests\StoreVehicleRequest;
 use App\Http\Requests\UpdateVehicleRequest;
+use App\Models\Vehicle;
 
 class VehicleController extends Controller
 {
